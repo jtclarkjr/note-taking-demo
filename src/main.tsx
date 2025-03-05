fa627d228d34dd4c7ff0e3419647323fc850418d
James <jtclarkjr@ucla.edu>
2025-03-05 11:31:14 +0900

    chore: move logo to assets

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { RouterProvider, createRouter } from '@tanstack/react-router'
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
 
-import './styles/styles.css'
+import '@/styles/styles.css'
 import reportWebVitals from './reportWebVitals.ts'
 
 // Create a new router instance
